e93bc29d77e00d1ee3efe686bbbfcce4221af7e4
Juwon Yi <juunewon@gmail.com>
2023-07-05 11:30:32 +0900

    git 생성

---
+++ src/main/java/com/goalapa/cacamuca/CacamucaApplication.java
@@ -0,0 +1,13 @@
+package com.goalapa.cacamuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacamucaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacamucaApplication.class, args);
+    }
+
+}
