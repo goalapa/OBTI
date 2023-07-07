50b9bb0afeb1e122370bbaa94f39ab5e78727bb6
Juwon Yi <juunewon@gmail.com>
2023-07-07 12:46:53 +0900

    도메인 패키지 수정

---
+++ src/main/java/com/goalapa/cacamuca/config/Config.java
@@ -0,0 +1,4 @@
+package com.goalapa.cacamuca.config;
+
+public class Config {
+}
--- src/main/java/com/goalapa/cacamuca/memberDomain/command/domain/aggregate/entity/Member.java
+++ src/main/java/com/goalapa/cacamuca/memberDomain/command/domain/aggregate/entity/Member.java
@@ -1,15 +1,13 @@
 package com.goalapa.cacamuca.memberDomain.command.domain.aggregate.entity;
 
-import com.goalapa.cacamuca.memberDomain.command.domain.aggregate.vo.PasswordVO;
-
 public class Member {
     private int memberNumber;
     private String memberId;
-    private PasswordVO memberPassword;
+    private String memberPassword;
     private String memberNickname;
     private String memberEmail;
     private String memberPermission;
     private String memberCountReport;
-    private String memberAge;
+    private String memberBirth;
 
 }
