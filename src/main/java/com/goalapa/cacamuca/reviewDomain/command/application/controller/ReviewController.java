96653a4c5f4bee3cb94147ccf14978819930a41a
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-07-11 17:16:29 +0900

    feat : 리뷰 사진 추가

--- src/main/java/com/goalapa/cacamuca/reviewDomain/command/application/controller/ReviewController.java
+++ src/main/java/com/goalapa/cacamuca/reviewDomain/command/application/controller/ReviewController.java
@@ -1,13 +1,22 @@
 package com.goalapa.cacamuca.reviewDomain.command.application.controller;
 
 import com.goalapa.cacamuca.reviewDomain.command.application.dto.ReviewDTO;
+import com.goalapa.cacamuca.reviewDomain.command.application.dto.ReviewPicDTO;
 import com.goalapa.cacamuca.reviewDomain.command.application.service.ReviewService;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 @Controller
 @RequestMapping("/*")
@@ -25,8 +34,9 @@ public class ReviewController {
     }
 
     @PostMapping("/review")
-    public String writeReview(HttpServletRequest request){
+    public String writeReview(HttpServletRequest request, @RequestParam List<MultipartFile> reviewPicUrl){
         ReviewDTO reviewDTO = new ReviewDTO();
+        ReviewPicDTO reviewPicDTO = new ReviewPicDTO();
 
         reviewDTO.setReviewContent(request.getParameter("reviewContent"));
         reviewDTO.setCountry(request.getParameter("country"));
@@ -37,7 +47,7 @@ public class ReviewController {
         reviewDTO.setReviewRate(Integer.parseInt(request.getParameter("reviewRate")));
         reviewDTO.setReviewLink(request.getParameter("review_link"));
 
-        reviewService.saveReview(reviewDTO);
+        reviewService.saveReview(reviewDTO, reviewPicUrl);
 
         return "redirect:/";
     }
--- src/main/java/com/goalapa/cacamuca/reviewDomain/command/application/service/ReviewService.java
+++ src/main/java/com/goalapa/cacamuca/reviewDomain/command/application/service/ReviewService.java
@@ -3,27 +3,61 @@ package com.goalapa.cacamuca.reviewDomain.command.application.service;
 import com.goalapa.cacamuca.reviewDomain.command.application.dto.ReviewDTO;
 import com.goalapa.cacamuca.reviewDomain.command.domain.aggregate.entity.Review;
 import com.goalapa.cacamuca.reviewDomain.command.domain.repository.ReviewRepository;
+import lombok.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.*;
 
 @Service
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
+
+
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
     @Transactional
-    public void saveReview(ReviewDTO reviewDTO) {
+    public void saveReview(ReviewDTO reviewDTO, List<MultipartFile> reviewPicUrl) {
         LocalDate date = LocalDate.now();
-
         Review review = new Review(reviewDTO.getReviewContent(), reviewDTO.getCountry(), reviewDTO.getFoodType(), reviewDTO.getFoodName(), date, reviewDTO.getReviewRate(), reviewDTO.getMemberNo(), reviewDTO.getFoodNo()
                                     , reviewDTO.getReviewKeyword(), reviewDTO.getReviewPrice(), reviewDTO.getReviewLink());
 
+        String root = "C:\\app-file";
+        String filePath = root + "/uploadFiles";
+
+        List<String> fileNames = new ArrayList<>();
+
+        for (MultipartFile file : reviewPicUrl) {
+            if (file.isEmpty()) {
+                continue;
+            }
+
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+            try {
+                String uniqueFileName = UUID.randomUUID().toString();
+                File uploadDir = new File(filePath);
+
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdirs();
+                }
+
+                String uploadPath = filePath + File.separator + uniqueFileName;
+                file.transferTo(new File(uploadPath));
+
+                fileNames.add(uniqueFileName);
+            } catch (IOException e) {
+            }
+        }
         reviewRepository.save(review);
     }
 }
