9a36ca4e1d9f0963e759f6905d2ea874a1e57bdf
sjpark <myrhymetree@gmail.com>
2023-07-12 16:34:40 +0900

    feat : 스프링 시큐리티

--- src/main/java/com/goalapa/cacamuca/config/SecurityConfig.java
+++ src/main/java/com/goalapa/cacamuca/config/SecurityConfig.java
@@ -1,24 +1,19 @@
 package com.goalapa.cacamuca.config;
 
-import com.goalapa.cacamuca.memberDomain.command.application.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.List;
-import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final AuthenticationService authenticationService;
+public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -26,38 +21,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/css/**", "/js/**" , "/images/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        Map<String, List<String>> permitListMap = authenticationService.getPermitListMap();
-        System.out.println("permitListMap = " + permitListMap);
-        List<String> adminPermitList = permitListMap.get("adminPermitList");
-        List<String> memberPermitList = permitListMap.get("memberPermitList");
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests()
-                .antMatchers(memberPermitList.toArray(new String[memberPermitList.size()])).hasAnyRole("MEMBER", "ADMIN")
-                .antMatchers(adminPermitList.toArray(new String[adminPermitList.size()])).hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/member/login")
-                .successForwardUrl("/")
-                .failureForwardUrl("/error/login")
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .logoutSuccessUrl("/")
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/error/denied");
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("login").authenticated()
+//                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+        return http.build();
     }
-
 }
