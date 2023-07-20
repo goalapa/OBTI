a2a677db9ebf583e0c21c96385654f82a39d97fc
Son Jung In <nanajungin13@gmail.com>
2023-07-20 20:07:40 +0900

    fix: (#96) foodDomain 엔티티 수정

--- src/main/java/com/goalapa/cacamuca/foodDomain/command/application/dto/FoodDTO.java
+++ src/main/java/com/goalapa/cacamuca/foodDomain/command/application/dto/FoodDTO.java
@@ -1,5 +1,85 @@
+
 package com.goalapa.cacamuca.foodDomain.command.application.dto;
+
+import com.goalapa.cacamuca.foodDomain.command.domain.aggregate.vo.Country;
+import com.goalapa.cacamuca.foodDomain.command.domain.aggregate.vo.FoodType;
 
 public class FoodDTO {
 
+    private int foodNo;
+    private String foodName;
+    private double foodRate;
+    private int foodPrice;
+    private Country country;
+    private FoodType foodType;
+
+    public FoodDTO() {}
+
+    public FoodDTO(String foodName, double foodRate, int foodPrice, Country country, FoodType foodType) {
+        this.foodName = foodName;
+        this.foodRate = foodRate;
+        this.foodPrice = foodPrice;
+        this.country = country;
+        this.foodType = foodType;
+    }
+
+    public int getFoodNo() {
+        return foodNo;
+    }
+
+    public void setFoodNo(int foodNo) {
+        this.foodNo = foodNo;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public double getFoodRate() {
+        return foodRate;
+    }
+
+    public void setFoodRate(double foodRate) {
+        this.foodRate = foodRate;
+    }
+
+    public int getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(int foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public FoodType getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(FoodType foodType) {
+        this.foodType = foodType;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodDTO{" +
+                "foodNo=" + foodNo +
+                ", foodName='" + foodName + '\'' +
+                ", foodRate=" + foodRate +
+                ", foodPrice=" + foodPrice +
+                ", country=" + country +
+                ", foodType=" + foodType +
+                '}';
+    }
 }
--- src/main/java/com/goalapa/cacamuca/foodDomain/command/application/dto/FoodPicDTO.java
+++ src/main/java/com/goalapa/cacamuca/foodDomain/command/application/dto/FoodPicDTO.java
@@ -1,4 +1,34 @@
 package com.goalapa.cacamuca.foodDomain.command.application.dto;
 
 public class FoodPicDTO {
+
+    private int foodNo;
+    private String foodPicUrl;
+
+    public FoodPicDTO() {}
+
+    public int getFoodNo() {
+        return foodNo;
+    }
+
+    public void setFoodNo(int foodNo) {
+        this.foodNo = foodNo;
+    }
+
+    public String getFoodPicUrl() {
+        return foodPicUrl;
+    }
+
+    public void setFoodPicUrl(String foodPicUrl) {
+        this.foodPicUrl = foodPicUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodPicDTO{" +
+                "foodNo=" + foodNo +
+                ", foodPicUrl='" + foodPicUrl + '\'' +
+                '}';
+    }
+
 }
