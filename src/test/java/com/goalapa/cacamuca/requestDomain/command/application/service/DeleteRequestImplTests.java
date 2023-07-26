3de0ce2252c3976d44ab99f70a1cdf9b7ea8d8a5
Son Jung In <nanajungin13@gmail.com>
2023-07-26 09:47:36 +0900

    test: saveStat 테스트

--- src/test/java/com/goalapa/cacamuca/requestDomain/command/application/service/DeleteRequestImplTests.java
+++ src/test/java/com/goalapa/cacamuca/requestDomain/command/application/service/DeleteRequestImplTests.java
@@ -1,4 +1,36 @@
 package com.goalapa.cacamuca.requestDomain.command.application.service;
 
+import com.goalapa.cacamuca.requestDomain.command.application.dto.RequestDTO;
+import com.goalapa.cacamuca.requestDomain.command.domain.repository.RequestRepository;
+import com.goalapa.cacamuca.statDomain.command.application.dto.StatDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
 public class DeleteRequestImplTests {
+
+    @Autowired
+    private DeleteRequestImpl deleteRequestImpl;
+
+    @Autowired
+    private RequestRepository requestRepository;
+    @DisplayName("요청 삭제 테스트")
+    @Test
+    void testDeleteRequest() {
+
+        long beforeSize = requestRepository.count();
+
+        deleteRequestImpl.deleteRequest(1);
+
+        long afterSize = requestRepository.count();
+
+        Assertions.assertEquals(beforeSize - 1, afterSize);
+
+    }
 }
